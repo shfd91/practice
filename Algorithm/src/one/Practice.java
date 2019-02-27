3c40f861bf0a11a8a11a0430df5fe67f55ffd865
shfd91 <shfd91@gmail.com>
2019-02-27 21:41:11 +0900

    Algorithm Project Commit

---
+++ Algorithm/src/one/Practice.java
@@ -0,0 +1,15 @@
+package one;
+
+import java.util.Scanner;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		System.out.println("입력하세요.");
+		stdIn.nextLine();
+		System.out.println(stdIn + "을 입력하였습니다.");
+
+	}
+
+}
